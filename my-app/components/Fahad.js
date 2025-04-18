0189380a2aaf06f6aed07c0bbb259403f92e7bbe
bilawal506 <mbilawalbhatti2009@gmail.com>
2025-04-18 12:25:42 +0500

    Initial commit

---
+++ my-app/components/Fahad.js
@@ -0,0 +1,8 @@
+function Fahad() {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline text-center">Fahad</h1>
+        </>
+    );
+}
+export default Fahad;
---
+++ my-app/components/Zain.js
@@ -0,0 +1,9 @@
+function Zain() {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline text-center">Zain</h1>
+        </>
+    );
+}
+
+export default Zain;
