c5c7dc627708f88b1cda1c6f7825a489b43e579a
zain-abbass <shrif27247@gmail.com>
2025-04-19 13:22:40 +0500

    google

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -2,7 +2,7 @@ import Fahad from "@/components/Fahad";
 import  Zain  from "@/components/Zain";
 export default function Home() {
   return (
-    <> <p>GoGaga</p>
+    <> <p>Google</p>
     <div>
       <h1 className="text-3xl font-bold underline text-center">Zain's Game</h1>
       <Zain />
