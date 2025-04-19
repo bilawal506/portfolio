234156c298b45aa87c0b5a19a87d69d1fd332663
Fahad225874 <fahadullah.388@gmail.com>
2025-04-19 13:58:06 -0700

    Changes made by Fahad

--- my-app/components/Fahad.js
+++ my-app/components/Fahad.js
@@ -2,6 +2,8 @@ function Fahad() {
     return (
         <>
             <h1 className="text-3xl font-bold underline text-center">Fahad</h1>
+            <button className="border border-black pl-3 pr-3 pt-1 pb-1 ml-20" >X</button>
+        
         </>
     );
 }
