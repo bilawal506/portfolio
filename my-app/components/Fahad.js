4d6da96967f18df48864e215d146a50b902f9995
bilawal506 <mbilawalbhatti2009@gmail.com>
2025-04-21 13:28:09 +0500

    Fahad button edit by muhammad

--- my-app/components/Fahad.js
+++ my-app/components/Fahad.js
@@ -2,7 +2,7 @@ function Fahad() {
     return (
         <>
             
-            <button className="border border-black pl-3 pr-3 pt-1 pb-1 cursor-pointer border-2" >X</button>
+            <button className="border-2 border-black pl-3 pr-3 pt-1 pb-1 cursor-pointer" >X</button>
         
         </>
     );
