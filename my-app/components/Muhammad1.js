48ee184d8bbd76bd6a8c2fba923164a71addb18d
Muhammad Md <mdm341250@gmail.com>
2025-04-21 12:51:02 +0500

    hi

---
+++ my-app/components/Muhammad1.js
@@ -0,0 +1,8 @@
+function Muhammad1() {
+    return (
+        <>
+            <button className="border border-black border2 pl-3 pr-3 pt-1 pb-1 cursor-pointer">X</button>
+        </>
+    );
+}
+export default Muhammad1;
