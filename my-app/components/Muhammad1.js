407d5616f78bd37b2bf455cb8da3b56628340993
Muhammad Md <mdm341250@gmail.com>
2025-04-21 13:30:45 +0500

    I can change the color of borders

--- my-app/components/Muhammad1.js
+++ my-app/components/Muhammad1.js
@@ -1,7 +1,8 @@
 function Muhammad1() {
     return (
         <>
-            <button className="p-2 border border-l-yellow-500  border-b-blue-500 border-r-red-500 cursor-pointer ">0</button>
+            <button className="p-2 border-3 border-t-orange-500 border-l-yellow-500  border-b-blue-500
+             border-r-green-500 cursor-pointer ">O</button>
         </>
     );
 }
