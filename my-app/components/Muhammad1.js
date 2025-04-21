34beff1c0670f04c6f4fb3d9f669953ec649d439
Muhammad Md <mdm341250@gmail.com>
2025-04-21 13:27:07 +0500

    Hy I create a button

--- my-app/components/Muhammad1.js
+++ my-app/components/Muhammad1.js
@@ -1,7 +1,7 @@
 function Muhammad1() {
     return (
         <>
-            
+            <button className="p-2 border border-l-yellow-500  border-b-blue-500 border-r-red-500 cursor-pointer ">0</button>
         </>
     );
 }
