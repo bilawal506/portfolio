561dfd33c222b455f137bc89154baaf1780f33c3
bilawal506 <mbilawalbhatti2009@gmail.com>
2025-04-21 12:55:52 +0500

    added Muhammad md

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,6 +1,7 @@
 import Fahad from "@/components/Fahad";
 import  Zain  from "@/components/Zain";
 import Muhammad from "@/components/Muhammad";
+import Muhammad1 from "@/components/Muhammad1";
 export default function Home() {
   return (
     <>
@@ -10,6 +11,7 @@ export default function Home() {
       <th className="px-4 py-2 text-3xl font-bold">Zain</th>
       <th className="px-4 py-2 text-3xl font-bold">Fahad</th>
       <th className="px-4 py-2 text-3xl font-bold">Muhammad</th>
+      <th className="px-4 py-2 text-3xl font-bold">Muhammad MD</th>
     </tr>
   </thead>
   <tbody>
@@ -17,6 +19,7 @@ export default function Home() {
       <td className="border px-4 py-2 text-center"><Zain /></td>
       <td className="border px-4 py-2 text-center"><Fahad /></td>
       <td className="border px-4 py-2 text-center"><Muhammad /></td>
+      <td className="border px-4 py-2 text-center"><Muhammad1 /></td>
     </tr>
   </tbody>
 </table>
