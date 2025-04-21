622ce8dc945ea454f227a3e4bec859c95e7433dc
bilawal506 <mbilawalbhatti2009@gmail.com>
2025-04-21 13:05:21 +0500

    recreate the MD button]

--- my-app/components/Muhammad1.js
+++ my-app/components/Muhammad1.js
@@ -1,7 +1,7 @@
 function Muhammad1() {
     return (
         <>
-            <button className="border border-black border2 pl-3 pr-3 pt-1 pb-1 cursor-pointer">X</button>
+            
         </>
     );
 }
