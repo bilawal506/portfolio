b68fd6373d74430784a26fd2735ae3f50ac4e0b3
bilawal506 <mbilawalbhatti2009@gmail.com>
2025-04-22 12:49:17 +0500

    O X Game State

--- usestate/app/page.tsx
+++ usestate/app/page.tsx
@@ -2,12 +2,19 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [count, setCount] = useState(0)
-
+  const [player, setplayer] = useState("X")
+  function handleClick(){
+    if (player === "O"){
+    setplayer("X")
+  }
+  else{
+    setplayer("O")
+  }
+  }
   return (
     <>
-
-    <h1>count = {count} </h1>
+    <h1>player = {player} </h1>
+    <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded cursor-pointer">Click to Increament Count</button>
     </>
   );
 }
