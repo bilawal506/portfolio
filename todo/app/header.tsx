fa18d9c94d6d295930e83007e57b5c1c2a34b0fc
bilawal506 <mbilawalbhatti2009@gmail.com>
2025-05-28 11:59:13 +0500

    ptaching

--- todo/app/header.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-    return (
-        <header className="flex justify-between items-center mb-4">
-            <h1 className="text-3xl font-bold">Todos</h1>
-        </header>
-    );
-}
